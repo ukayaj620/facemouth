6b120ddd78096e793058f3ce9bf63f03af36e9f5
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-10-15 14:22:29 +0700

    feat: proper masking using morphology

--- facial_extractor/face_extractor.py
+++ facial_extractor/face_extractor.py
@@ -13,10 +13,16 @@ class FaceExtractor:
         self.image_hsv = cv2.cvtColor(self.image_bgr, cv2.COLOR_BGR2HSV)
 
     def get_face_mask(self):
-        lower = np.array([0, 48, 80], dtype="uint8")
+        lower = np.array([0, 48, 10], dtype="uint8")
         upper = np.array([20, 255, 255], dtype="uint8")
 
-        return cv2.inRange(self.image_hsv, lower, upper)
+        mask = cv2.inRange(self.image_hsv, lower, upper)
+
+        kernel_open = np.ones((9, 9), dtype=np.uint8)
+        mask_open = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel_open)
+
+        kernel_close = np.ones((199, 199), dtype=np.uint8)
+        return cv2.morphologyEx(mask_open, cv2.MORPH_CLOSE, kernel_close)
 
     def apply_mask(self, mask):
         return cv2.bitwise_and(self.image_bgr, self.image_bgr, mask=mask)
