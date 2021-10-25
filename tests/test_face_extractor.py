8a7c19521bb99b9e577d118a4b27df07d97ac736
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-10-25 13:25:14 +0700

    feat: test facemask personal

--- tests/test_face_extractor.py
+++ tests/test_face_extractor.py
@@ -67,9 +67,23 @@ class TestFaceExtractor(unittest.TestCase):
             plt.imshow(cv2.resize(image, (300, 400)))
         
         plt.show()
+    
+    def test_face_mask_personal(self):
+        name = "kevin"
+        fig = plt.figure(figsize=(10, 12))
+        columns = 3
+        rows = 2
+        for i in range(1, columns * rows + 1):
+            self.face_extractor.load_image(
+                "./data/{}/foto_{}.jpg".format(name, i))
+            image = self.face_extractor.get_face_mask()
+            fig.add_subplot(rows, columns, i)
+            plt.imshow(cv2.resize(image, (300, 400)), cmap="gray")
+
+        plt.show()
 
     def test_apply_mask_personal(self):
-        name = "jayaku"
+        name = "martien"
         fig = plt.figure(figsize=(10, 12))
         columns = 3
         rows = 2
@@ -84,7 +98,7 @@ class TestFaceExtractor(unittest.TestCase):
         plt.show()
 
     def test_face_contour_personal(self):
-        name = "jayaku"
+        name = "martien"
         fig = plt.figure(figsize=(10, 12))
         columns = 3
         rows = 2
