bd438a5a0ba412e26f74394897161b3512c3479a
ferdyferdy9 <ferdyferdy9514@gmail.com>
2021-10-27 10:28:37 +0700

    Create config.py

---
+++ gui/config.py
@@ -0,0 +1,14 @@
+DEFAULT_FONT = "Verdana"
+FONT_SIZE_H1 = 20
+FONT_SIZE_NORMAL = 12
+
+DEFAULT_PAD_X = 10
+DEFAULT_PAD_Y = 10
+
+DEFAULT_CANVAS_WIDTH = 320
+DEFAULT_CANVAS_HEIGHT = 320
+
+IMAGE_FILE_TYPES = (
+    ('Image files', '*.png *.jpg *.jpeg *.bmp *.tiff *.webm *.gif'),
+    ('All files', '*.*')
+)
